ecdb17dd5df75e198e33431efc57e2341b8c7af9
JAGJ10 <jagj10@gmail.com>
2014-10-29 15:28:11 -0400

    Updated particle

--- Particle.java
+++ Particle.java
@@ -1,3 +1,45 @@
 public class Particle {
-	private Vector3D pos;
+	private Vector3D oldPos;
+	private Vector3D newPos;
+	private Vector3D velocity;
+	private Vector3D force;
+	private double mass;
+	private double lambda;
+	private double pConstraint;
+	private ArrayList<Particle> neighbors;
+
+	public Particle(Vector3D pos, float mass) {
+		this.oldPos = pos;
+		this.mass = mass;
+		velocity = new Vector3D(0, 0, 0);
+		force = new Vector3D(0, 0, 0);
+		newPos = new Vector3D(0, 0, 0);
+	}
+
+	public Vector3D getOldPos() {
+		return oldPos;
+	}
+
+	public Vector3D getNewPos() {
+		return newPos;
+	}
+
+	public Vector3D getForce() {
+		return force;
+	}
+
+	public double getMass() {
+		return mass;
+	}
+
+	public ArrayList<Particle> getNeighbors() {
+		return neighbors;
+	}
+
+	public double getPConstraint() {
+		return pConstraint;
+	}
+
+	
+
 }
