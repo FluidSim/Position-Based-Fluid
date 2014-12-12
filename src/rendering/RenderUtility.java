e7f49d09ffe6413a2166a0121c85a584a60e0706
JAGJ10 <jagj10@gmail.com>
2014-12-12 13:47:14 -0500

    fixed render utility

--- src/rendering/RenderUtility.java
+++ src/rendering/RenderUtility.java
@@ -16,25 +16,25 @@ public final class RenderUtility {
 	
 	/** Add a Vector3 as a uniform */
 	public static void addVector3(ShaderHelper shader, Vector3 V, String name){
-		int loc = GL20.glGetUniformLocation(shader.getProgramId(), name);
+		int loc = GL20.glGetUniformLocation(shader.getProgram(), name);
 		GL20.glUniform3f(loc, V.x, V.y, V.z);
 	}
 	
 	/** Add a Vector2 as a uniform */
 	public static void addVector2(ShaderHelper shader, Vector2 V, String name){
-		int loc = GL20.glGetUniformLocation(shader.getProgramId(), name);
+		int loc = GL20.glGetUniformLocation(shader.getProgram(), name);
 		GL20.glUniform2f(loc, V.x, V.y);
 	}
 	
 	/** Add an int as a uniform */
 	public static void addInt(ShaderHelper shader, int x, String name){
-		int loc = GL20.glGetUniformLocation(shader.getProgramId(), name);
+		int loc = GL20.glGetUniformLocation(shader.getProgram(), name);
 		GL20.glUniform1i(loc,x);
 	}
 	
 	/** Add a float as a uniform */
 	public static void addFloat(ShaderHelper shader, int f, String name){
-		int loc = GL20.glGetUniformLocation(shader.getProgramId(), name);
+		int loc = GL20.glGetUniformLocation(shader.getProgram(), name);
 		GL20.glUniform1f(loc, f);
 	}
 	
@@ -43,7 +43,7 @@ public final class RenderUtility {
 		FloatBuffer Mbuffer = BufferUtils.createFloatBuffer(M.m.length);
 		Mbuffer.put(M.m);
 		Mbuffer.flip();
-		int loc = GL20.glGetUniformLocation(shader.getProgramId(), name);
+		int loc = GL20.glGetUniformLocation(shader.getProgram(), name);
 		GL20.glUniformMatrix4(loc, true, Mbuffer);
 	}
 	
